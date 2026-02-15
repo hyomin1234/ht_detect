bf72e9a5180ccc170e4f1eba1a135bfd08dfafa5
hyomin1234 <rhgyals1008@gmail.com>
2026-02-15 16:52:09 +0900

    Delete dummy.v

--- dummy.v
+++
@@ -1,9 +0,0 @@
-module top (
-    input A,
-    input B,
-    output Z
-);
-wire w1;
-AND2_X1 u1 (.A(A), .B(B), .Z(w1)); // Driver w1
-INV_X1 u2 (.A(w1), .Z(Z));         // Load w1, Driver Z
-endmodule
